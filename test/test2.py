536cf72b967ab53059d4bdd74247c8eb51f205a1
Thomas Liao <tliao311@gmail.com>
2016-02-11 16:07:52 -0500

    Create a test folder and file for ps2

---
+++ test/test2.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestPS2Methods(unittest.TestCase):
+
+    def test_nullargs(self):
+        self.assertEqual()
+
+    def test_lw(self):
+        self.assertEqual()
+
+if __name__ == '__main__':
+    unittest.main()
